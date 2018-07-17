370041f2afc5b46d2ed55b084bd0a9da25f95739
Shhoya <hunho88@gmail.com>
2018-07-17 18:07:33 +0900

    h

---
+++ SourceCode/01_C++/ClassnArray/PointArr1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+	int x, y;
+
+public:
+	Point()
+	{
+		cout << "Point() Call" << endl;
+		x = y = 0;
+	}
+	Point(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+
+	int GetX() { return x; }
+	int GetY() { return y; }
+	void SetX(int _x) { x = _x; }
+	void SetY(int _y) { y = _y; }
+};
+
+int main()
+{
+	Point arr[5];
+
+	for (int i = 0; i < 5;i++)
+	{
+		arr[i].SetX(i * 2);
+		arr[i].SetY(i * 3);
+	}
+
+	for (int j = 0; j < 5; j++)
+	{
+		cout << "x: " << arr[j].GetX() << " ";
+		cout << "y: " << arr[j].GetY() << endl;
+	}
+	return 0;
+}
