edd11e69680cbbaabd88c2341eb40b610c698132
Shhoya <hunho88@gmail.com>
2018-07-19 23:30:50 +0900

    h

---
+++ SourceCode/Mobilept/Mobilept/android.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+class Android
+{
+
+public:
+	void Android_s();
+	void AndroidMenu();
+	void a_Decompile();
+	void a_Data();
+	
+};
---
+++ SourceCode/Mobilept/Mobilept/ios.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+
+
+using namespace std;
+
+class iOS
+{
+
+public:
+	void iOS_s();
+	void iOSMenu();
+	void Decrypt();
+	void Data();
+};
+
+
